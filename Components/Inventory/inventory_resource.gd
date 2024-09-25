211665929d067b1f7a592a68f17c165abd8afb41
CreativeBuissnessCompany <sofaclubmusic@gmail.com>
2024-09-25 11:46:30 -0700

    Qty seems to be working on pickups
    Waiting on Buy Sell or Drop buttons to remove items from inventory UI and Vending machine UI

--- Components/Inventory/inventory_resource.gd
+++ Components/Inventory/inventory_resource.gd
@@ -9,26 +9,45 @@ extends Resource
 
 func add_item(item:Item):
 	
+	# Check Qty ...
 	if _content.size() != 0:
+		var count = _content.size()
+		#print(" First Loop .....")
+		#print("Count Size : %s" % count)
 		for i in _content:
-			print("Itterate...")
-			print(i.name)
-			print(item.name)
+			#print("for i in _content loop")
+			#print(i.name)
+			#print(item.name)
 			if i.name == item.name:
-				print("More Than One...")
+				print("Names Match ..... ")
 				#print(i.name)
 				i.qty += 1
 				print("Qty : " + str(i.qty))
+				break
 			else:
-				_content.append(item)
-				
-				print(" Total Content : ")
-				print(_content)
-				
+				count -= 1
+				#print("Count ")
+				#print(count)
+				if count == 0:
+					_content.append(item)
+					#print(" All Items in Inventory Checked")
+					break
+					
 	else:
+		#print(" First Item Onlu")
 		_content.append(item)
 
+
+
+
 func remove_item(item:Item):
+	
+	# NOTE Need a drop or use button from Naz ...
+	
+	# Check Qty ...
+	
+	
+	
 	_content.erase(item)
 	
 
