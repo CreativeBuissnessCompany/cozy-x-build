b5fb38190e340823a367dffb57d8ddc1c99c7c3e
CreativeBuissnessCompany <sofaclubmusic@gmail.com>
2024-09-25 16:29:20 -0700

    BedTime update... Needs tweaks
    bed is now two separate PNG. player slides in for bed, .....Need to turn on collision after maybe?

--- Entities/Items/Interactive_Furniture/Beds/bed.gd
+++ Entities/Items/Interactive_Furniture/Beds/bed.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 
 var player : Player
 @onready var dialog_box: ColorRect = %DialogBox
+@onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
 
 
 
@@ -27,6 +28,18 @@ func _on_yes_pressed() -> void:
 	time_tracker.day += 1
 	dialog_box.hide()
 	print("pressed")
+	
+	# Disable Collisions so player can get in bed...
+	collision_shape_2d.disabled = true
+	# Move player under sheets?
+	player.z_index = 1
+	player.global_position += Vector2(50.0,-20.0)
+	# Timer
+	await get_tree().create_timer(3).timeout
+	# Outta Bed
+	player.z_index = 0
+	player.global_position += Vector2(-50.0,20.0)
+	
 	pass # Replace with function body.
 
 
