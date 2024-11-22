08c5745f5c32cacd28c64687c269a1f0a534d225
CreativeBuissnessCompany <sofaclubmusic@gmail.com>
2024-11-21 17:50:39 -0800

    backup before installing resource database plugin

--- components/asset_flipper/asset_flipper.gd
+++ components/asset_flipper/asset_flipper.gd
@@ -1,14 +1,17 @@
 extends Sprite2D
 
 
+@export var assets_options: Array[Texture2D]
+
+
 
 enum ASSET {ONE, TWO, THREE}
+
 @export var switch_asset_to: ASSET = ASSET.ONE:
 	set(value):
 		switch_asset_to = value
 		switch_asset()
 
-@export var assets_options: Array[Texture2D]
 
 
 
@@ -18,5 +21,5 @@ func _ready() -> void:
 	
 func switch_asset():
 	print("poop")
-
+	await Engine.get_main_loop().process_frame
 	self.texture = assets_options[switch_asset_to]
